b779268cbb07d07543ee497a531820592e15961c
Andre Guedes <andre.guedes@intel.com>
2016-05-05 11:07:02 -0300

    quark-se-devboard: Organize contiki-conf.h
    This patch organizes the contiki-conf.h files so related options are
    grouped together.
    
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>

--- platform/quark-se-devboard/contiki-conf.h
+++ platform/quark-se-devboard/contiki-conf.h
@@ -52,70 +52,56 @@ typedef uint32_t rtimer_clock_t;
  */
 #define LEDS_CONF_ALL (3)
 
-#define UIP_CONF_BUFFER_SIZE              1280
-
-#define NETSTACK_CONF_NETWORK sicslowpan_driver
-
-#define NETSTACK_CONF_MAC     csma_driver
-
-#define NETSTACK_CONF_RDC     nullrdc_driver
-
-/* Configure NullRDC for when it's selected */
-#define NULLRDC_CONF_802154_AUTOACK                  1
-#define NULLRDC_CONF_802154_AUTOACK_HW               1
-
+#define NETSTACK_CONF_RADIO                     cc2520_driver
+#define NETSTACK_CONF_FRAMER                    framer_802154
+#define NETSTACK_CONF_RDC                       nullrdc_driver
+#define NETSTACK_CONF_MAC                       csma_driver
+#define NETSTACK_CONF_NETWORK                   sicslowpan_driver
+#define NETSTACK_CONF_WITH_IPV6                 1
 #define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE    8
-
-#define NETSTACK_CONF_FRAMER  framer_802154
-
-#define NETSTACK_CONF_RADIO   cc2520_driver
-
-#define CC2520_CONF_AUTOACK              1
-
-#define RF_CHANNEL 26
-
-#define LINKADDR_CONF_SIZE              8
-
-#define UIP_CONF_LL_802154              1
-#define UIP_CONF_LLH_LEN                0
-
-#define UIP_CONF_ROUTER                 1
-
-/* configure number of neighbors and routes */
-#define NBR_TABLE_CONF_MAX_NEIGHBORS     30
-#define UIP_CONF_MAX_ROUTES   30
-
-#define UIP_CONF_ND6_SEND_NA    0
-#define UIP_CONF_ND6_SEND_RA    0
-#define UIP_CONF_ND6_REACHABLE_TIME     600000
-#define UIP_CONF_ND6_RETRANS_TIMER      10000
-
-#define NETSTACK_CONF_WITH_IPV6                   1
-#define UIP_CONF_IPV6_QUEUE_PKT         0
-#define UIP_CONF_IPV6_CHECKS            1
-#define UIP_CONF_IPV6_REASSEMBLY        0
-#define UIP_CONF_NETIF_MAX_ADDRESSES    3
-#define UIP_CONF_IP_FORWARD             0
 
 #define SICSLOWPAN_CONF_COMPRESSION             SICSLOWPAN_COMPRESSION_HC06
 #define SICSLOWPAN_CONF_FRAG                    1
 #define SICSLOWPAN_CONF_MAXAGE                  8
 #define SICSLOWPAN_CONF_MAX_ADDR_CONTEXTS       2
 
-#define UIP_CONF_LLH_LEN         0
-#define UIP_CONF_RECEIVE_WINDOW  48
-#define UIP_CONF_TCP_MSS         48
-#define UIP_CONF_MAX_CONNECTIONS 4
-#define UIP_CONF_MAX_LISTENPORTS 8
-#define UIP_CONF_UDP_CONNS       12
-#define UIP_CONF_BROADCAST       1
-#define UIP_ARCH_IPCHKSUM        1
-#define UIP_CONF_UDP             1
-#define UIP_CONF_UDP_CHECKSUMS   1
-#define UIP_CONF_PINGADDRCONF    0
-#define UIP_CONF_LOGGING         0
+#define NBR_TABLE_CONF_MAX_NEIGHBORS            30
 
-#define UIP_CONF_TCP_SPLIT       0
+#define UIP_CONF_BUFFER_SIZE                    1280
+#define UIP_CONF_LL_802154                      1
+#define UIP_CONF_LLH_LEN                        0
+#define UIP_CONF_ROUTER                         1
+#define UIP_CONF_MAX_ROUTES                     30
+#define UIP_CONF_ND6_SEND_NA                    0
+#define UIP_CONF_ND6_SEND_RA                    0
+#define UIP_CONF_ND6_REACHABLE_TIME             600000
+#define UIP_CONF_ND6_RETRANS_TIMER              10000
+#define UIP_CONF_IPV6_QUEUE_PKT                 0
+#define UIP_CONF_IPV6_CHECKS                    1
+#define UIP_CONF_IPV6_REASSEMBLY                0
+#define UIP_CONF_NETIF_MAX_ADDRESSES            3
+#define UIP_CONF_IP_FORWARD                     0
+#define UIP_CONF_RECEIVE_WINDOW                 48
+#define UIP_CONF_TCP_MSS                        48
+#define UIP_CONF_MAX_CONNECTIONS                4
+#define UIP_CONF_MAX_LISTENPORTS                8
+#define UIP_CONF_UDP_CONNS                      12
+#define UIP_CONF_BROADCAST                      1
+#define UIP_ARCH_IPCHKSUM                       1
+#define UIP_CONF_UDP                            1
+#define UIP_CONF_UDP_CHECKSUMS                  1
+#define UIP_CONF_PINGADDRCONF                   0
+#define UIP_CONF_LOGGING                        0
+#define UIP_CONF_TCP_SPLIT                      0
+
+#define NULLRDC_CONF_802154_AUTOACK             1
+#define NULLRDC_CONF_802154_AUTOACK_HW          1
+
+#define CC2520_CONF_AUTOACK                     1
+
+#define RF_CHANNEL                              26
+
+#define LINKADDR_CONF_SIZE                      8
 
 /* Include the project config */
 /* PROJECT_CONF_H might be defined in the project Makefile */
